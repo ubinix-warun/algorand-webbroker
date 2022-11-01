02fd5d4e62a61c82085c60506efea387ba558bf9
ubinix-warun <warun@ubinix.com>
2022-11-01 23:33:42 +0700

    ADD: hook-receiver

---
+++ backends/hook-receiver/index.js
@@ -0,0 +1,15 @@
+// index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+app.post('/post-test', (req, res) => {
+    console.log('Got body:', req.body);
+    res.sendStatus(200);
+});
+
+app.listen(8080, () => console.log(`Started server at http://localhost:8080!`));
